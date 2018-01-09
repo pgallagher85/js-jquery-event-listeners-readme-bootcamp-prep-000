bfce72dbe6d39bc67d29d590691475570e9cdfb7
Patrick Gallagher <pgallagher85@gmail.com>
2018-01-09 16:12:11 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -18,7 +18,7 @@ function pressIt() {
 
 function submitIt() {
   $('form').on("submit", function() {
-    alerts("Your form is going to be submitted now.");
+    alert("Your form is going to be submitted now.");
   })
 }
 
