0fcbaf8fb5ce93978cf2e21168f8f13a611426aa
Patrick Gallagher <pgallagher85@gmail.com>
2018-01-09 16:10:52 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -8,6 +8,14 @@ function frameIt() {
   $('img').addClass("tasty")
   }
 
+function pressIt() {
+  $('form').on("keydown", function(key) {
+    if (key.which == 71) {
+      alert("You pressed the G key.");
+    }
+  })
+}
+
 $(document).ready(function(){
 
 // call functions here
